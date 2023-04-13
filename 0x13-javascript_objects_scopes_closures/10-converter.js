3cb4df3856f5c6a59fea8c2de7424880b49e61de
Onah-NeLson <Onahnelson2@gmail.com>
2023-04-13 14:24:33 -0700

    javascript objs

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,6 +1,7 @@
  #!/usr/bin/node
 // hannds on javascript objs
-
 exports.converter = function (base) {
-  return (res) => res.toString(base);
+  return function (number) {
+    return (number >>> 0).toString(base);
+  };
 };
