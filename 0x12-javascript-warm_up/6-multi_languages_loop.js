7a02a18f6f85c4c6fb33409bf258518ada4785d4
Onah-NeLson <Onahnelson2@gmail.com>
2023-04-13 17:12:37 -0700

    javascript is Amazing

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// This script prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+
+while (array[i]) {
+  console.log(array[i]);
+  i++;
+}
