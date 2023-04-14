7770d4a4d5f6903e7f68462e6163c7e0aa19f930
Onah-NeLson <Onahnelson2@gmail.com>
2023-04-13 17:45:31 -0700

    This script computes and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// This script computes and prints a factorial
+function factorial (x) {
+  if (isNaN(x) | x === 1) {
+    return (1);
+  } else {
+    return (x * factorial(x - 1));
+  }
+}
+
+console.log(factorial(process.argv[2]));
