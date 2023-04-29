137d97c245602fa42c768095e8ceac0e0ce60e3d
Onah-NeLson <Onahnelson2@gmail.com>
2023-04-29 08:17:54 -0700

    python-network_1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""a Python script that takes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id in the response header
+"""
+a Python script that takes in a URL, sends a request to the URL and displays the value of the variable X-Request-Id in the response header
 """
 import sys
 import requests
