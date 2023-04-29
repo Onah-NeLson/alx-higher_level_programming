0b333e7fad5800759c446355be7ebd758e1dda4b
Onah-NeLson <Onahnelson2@gmail.com>
2023-04-29 14:33:45 -0700

    python-network_1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""This Script Uses the GitHub API to display a GitHub ID based on given credentials.
+"""
+This Script Uses the GitHub API to display a
+GitHub ID based on given credentials.
 Usage: ./10-my_github.py <GitHub username> <GitHub password>
   - Uses Basic Authentication to access the ID.
 """
