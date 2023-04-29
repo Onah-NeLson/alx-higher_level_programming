0ac279641f0fa56e8f1c4b1195ec7d194ecf17ca
Onah-NeLson <Onahnelson2@gmail.com>
2023-04-29 15:47:38 -0700

    python-network_1

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""
-a Python script that takes in a URL,
-sends a request to the URL and 
+"""a Python script that takes in a URL,
+sends a request to the URL and
 displays the value of the variable X-Request-Id in the response header
 Usage: ./5-hbtn_header.py <URL>
 """
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,12 +2,12 @@
 """
 This script,Sends a POST request to a
 given URL with a given email.
+
 Usage: ./6-post_email.py <URL> <email>
 - Displays the body of the response.
 """
-import requests
 import sys
-
+import requests
 
 
 if __name__ == "__main__":
