eb0f36c96a410b1f3c22f729840b3b128253c03b
Onah-NeLson <Onahnelson2@gmail.com>
2023-04-29 14:56:24 -0700

    python-network_1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This script,Sends a POST request to a given URL with a given email.
+This script,Sends a POST request to a
+given URL with a given email.
 Usage: ./6-post_email.py <URL> <email>
 - Displays the body of the response.
 """
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""This script Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+"""
+This script Sends a POST request to 
+http://0.0.0.0:5000/search_user with a given letter.
 Usage: ./8-json_api.py <letter>
   - The letter is sent as the value of the variable `q`.
   - If no letter is provided, sends `q=""`.
