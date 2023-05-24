2278fc7474118b8e75fc8af224cf26f04abfb529
Onah-NeLson <Onahnelson2@gmail.com>
2023-05-24 07:30:43 -0700

    web_scraping

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// This script displays the status code of a GET request.
+
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, (error, response) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
