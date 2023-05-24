796494750b9ea7215b065b4b6cdfc5a74e3c7771
Onah-NeLson <Onahnelson2@gmail.com>
2023-05-24 06:44:19 -0700

    web_scraping

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// This script reads and prints the content of a file.
+
+const fs = require('fs');
+const filename = process.argv[2];
+
+fs.readFile(filename, 'utf-8', (error, content) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(content);
+  }
+});
