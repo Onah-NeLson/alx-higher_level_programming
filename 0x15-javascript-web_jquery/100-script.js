11f542b7c1559b12aa84204d6f930f4e198ba4f9
Onah-NeLson <Onahnelson2@gmail.com>
2023-05-31 16:23:50 -0700

    js_jquery

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+		    document.querySelector('HEADER').style.color = '#FF0000';
+		      });
+
+// a JavaScript script that updates the text color of the <header> element to red (#FF0000)
